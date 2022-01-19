536ccec687ff37ecbab6d35e12e4df634e32e77b
priyabhard <97886925+priyabhard@users.noreply.github.com>
2022-01-19 15:02:04 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,33 @@
+for windows:
+  variable "instance_type" {
+    description = "instance type"
+    type        = string
+    default     = "t2.micro"
+  }
+  variable "ec2_ami" {
+    description = "ami id"
+    type        = string
+    default     = "ami-0d44dd65e2051066e"
+  }
+  variable "instance_name" {
+    type        = string
+    default     = "windows"
+  }
+  
+for linux:
+  variable "instance_type" {
+    description = "instance type"
+    type        = string
+    default     = "t2.micro"
+  }
+  variable "ec2_ami" {
+    description = "ami id"
+    type        = string
+    default     = "ami-08eecc65e6189066d"
+  }
+  variable "instance_name" {
+    type        = string
+    default     = "linux"
+  }
+  
+  
