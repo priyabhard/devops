bb63f439185270b763eb1d4e4e267650a4063d48
priyabhard <97886925+priyabhard@users.noreply.github.com>
2022-01-19 15:19:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
   region = "us-east-1"
   }
  
-for windows:
+# for windows:
   resource "aws_instance" "my-ec2"{
   instance_type=var.instance_type
   ami = var.ec2_ami
@@ -11,7 +11,7 @@ for windows:
   }
   }
   
-for linux:
+# for linux:
   resource "aws_instance" "my-ec2"{
   instance_type=var.instance_type
   ami = var.ec2_ami
