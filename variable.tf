5adf69ec690572cba9b5c5f3eeabda3948bc7fd4
priyabhard <97886925+priyabhard@users.noreply.github.com>
2022-01-24 12:01:49 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,20 @@
 variable "instance_type" {
     description = "instance type"
-    type        = list(string)
+    type        = string
     default     = "t2.micro"
   }
 
   variable "ec2_ami" {
-    default={"linux" = "ami-08e4e35cccc6189f4" && "linux" = "ami-083602cee93914c0c"}
-   {"windows" = "ami-0d43d465e2051057f" && "ami-0b2048a9d4fca3418"}
+    default={ "linux" = "ami-08e4e35cccc6189f4"
+   "windows" = "ami-0d43d465e2051057f"
+    }
   }
 
- variable "instance_names" {
-  description = "creating ec2 instances"
-  type        = list(string)
-  default     = ["instance1", "instance2"]
-}
+  variable "name" {
+    type        = string
+    default     = "Ec2"
+  }
+
   
 
   
